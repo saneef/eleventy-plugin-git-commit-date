b7fede02d8eaffe444295d0c5813b521abce9ced
Saneef Ansari <hello@saneef.com>
2026-02-27 12:49:19 +0530

    feat: query git repo only once
    The builds are much faster with these change!

--- src/getGitCommitDateFromPath.js
+++ src/getGitCommitDateFromPath.js
@@ -1,34 +1,23 @@
 // @ts-check
-import memoize from "./utils/memoize.js";
-import { spawnAsync } from "./utils/spawn.js";
+import { getRepoGitCommitDateMap } from "./utils/git.js";
+
+async function initGetGitCommitDateFromPath() {
+  const fileMap = await getRepoGitCommitDateMap();
+
+  function getGitCommitDateFromPath(filePath) {
+    return fileMap.get(filePath);
+  }
+
+  return getGitCommitDateFromPath;
+}
 
 /**
  * Gets the Git commit date from path.
  *
- * The code is based on @vuepress/plugin-last-updated,
- * https://github.com/vuejs/vuepress/blob/master/packages/%40vuepress/plugin-last-updated/
- *
  * @param {string} filePath The file path
- * @returns {Promise<Date | undefined>} Commit date if path is commited to Git,
- *   otherwise `undefined`
+ * @returns {Date | undefined} Commit date if path is commited to Git, otherwise
+ *   `undefined`
  */
-async function getGitCommitDateFromPath(filePath) {
-  let output;
+const getGitCommitDateFromPath = await initGetGitCommitDateFromPath();
 
-  try {
-    output = await spawnAsync("git", ["log", "-1", "--format=%at", filePath]);
-  } catch (e) {
-    console.log(e);
-    throw new Error("Fail to run 'git log'");
-  }
-
-  const ts = parseInt(output, 10) * 1000;
-
-  // Paths not commited to Git returns empty timestamps, resulting in NaN.
-  // So, convert only valid timestamps.
-  if (!isNaN(ts)) {
-    return new Date(ts);
-  }
-}
-
-export default memoize(getGitCommitDateFromPath);
+export default getGitCommitDateFromPath;
